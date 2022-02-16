674058fb090bd5809175f80561e14e402a733cf4
bee95 <dcb95@naver.com>
2022-02-16 18:07:34 +0900

    검색창

--- js/search_box.js
+++ js/search_box.js
@@ -1,8 +1,27 @@
-function check(){
-    if(document.search.keyWord.value==''){
-        alert('검색어를 입력하세요'); 
-        document.search.keyWord.focus();
-        return false; 
-        
-    } 
+const Searching = Search.prototype;
+
+function Search(){
+    this.keyword = document.querySelector('input[name = "search"]');
+    this.engine = document.querySelector('.SelectSearch');
+    this.button = document.querySelector('.img-button');
+    this.form = document.querySelector('.search');
+
+    this.Engine();
 }
+Searching.Engine = function(){
+    this.form.addEventListener('submit', e => {
+    
+        e.preventDefault();
+        
+        let engine = this.engine.value;  
+        let keyword = this.keyword.value;
+
+        if(engine === 'google'){
+            location.href = 'https://www.google.co.kr/search?q=' + keyword;
+        }else if(engine === 'naver'){
+            location.href = 'https://search.naver.com/search.naver?query=' + keyword;
+        }
+    });
+}
+
+new Search();
